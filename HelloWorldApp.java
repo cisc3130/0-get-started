da1ea95c1786a5604560b26dffe2e7f2d21f483e
Lalla22 <47159210+Lalla22@users.noreply.github.com>
2019-02-05 23:04:37 -0500

    Update HelloWorldApp.java

---
+++ HelloWorldApp.java
@@ -0,0 +1,13 @@
+/**
+ * Hello World Homework Github 
+ *
+ * @author Lalla Sankara 
+ * @version 2/5/2019
+ */
+public class HelloWorld
+{
+    public static void main (String args[] )
+    {
+        System.out.println("Hello World App");
+    }
+}
